df0f946b323cca18c97b2a5fe7300fff726365d0
AmirulAshraff23 <ashraffatwork92@gmail.com>
2024-02-22 15:27:35 +0800

    ss

--- src/app.js
+++ src/app.js
@@ -4,6 +4,35 @@ let gender;
 let Weight;
 let height;
 
+function calculateBMI(event) {
+
+    event.preventDefault();
+
+
+    // Get user input from the form
+    weight = parseFloat(document.getElementById('weightInput').value);
+    height = parseFloat(document.getElementById('heightInput').value);
+
+    // Check if input is valid
+    if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
+        alert('Please enter valid weight and height.');
+        return;
+    }
+
+    // Calculate BMI
+    BMI = 0;
+
+    // Display BMI in console
+    console.log('BMR:', BMR);
+
+    // Display results
+    document.getElementById('resultWeight').innerText = weight.toFixed(2);
+    document.getElementById('resultHeight').innerText = height.toFixed(2);
+    document.getElementById('resultBMR').innerText = BMR.toFixed(2);
+
+    
+}
+
 function resetForm() {
     // Clear user input and results
     document.getElementById('ageInput').value = '';
